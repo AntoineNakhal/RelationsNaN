c3233bb72a18791d8634beb2fb8e90ff611a1efe
Antoine Nakhal <2156190@cegepmontpetit.ca>
2025-09-02 17:41:19 -0400

    GOGOGO

---
+++ Controllers/GamesController.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RelationsNaN.Data;
+using RelationsNaN.Models;
+
+namespace RelationsNaN.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly RelationsNaNContext _context;
+
+        public GamesController(RelationsNaNContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Games
+        public async Task<IActionResult> Index()
+        {
+            var relationsNaNContext = _context.Game.Include(g => g.Genre).Include(p => p.Platforms);
+            return View(await relationsNaNContext.ToListAsync());
+        }
+
+        // GET: Games/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Game
+                .Include(g => g.Genre)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        // GET: Games/Create
+        public IActionResult Create()
+        {
+            ViewData["GenreId"] = new SelectList(_context.Genre, "Name", "Name");
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Image,ReleaseYear,GenreId")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(game);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GenreId"] = new SelectList(_context.Genre, "Name", "Name", game.Genre);
+            return View(game);
+        }
+
+        // GET: Games/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Game.Include(g => g.Platforms).FirstOrDefaultAsync(g => g.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            ViewData["GenreId"] = new SelectList(_context.Genre, "Id", "Name", game.Genre);
+            ViewData["Platforms"] = new SelectList(_context.Platform.OrderBy(p => p.Name), "Id", "Name");
+            return View(game);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Image,ReleaseYear,GenreId")] Game game)
+        {
+            if (id != game.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(game);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GameExists(game.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GenreId"] = new SelectList(_context.Genre, "Id", "Name", game.Genre);
+            return View(game);
+        }
+
+        // POST: Games/AddPlatform
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddPlatform(int id, int platformId)
+        {
+            // Charger le jeu avec ses plateformes actuelles
+            var game = await _context.Game
+                .Include(p=> p.Genre)
+                .Include(g => g.Platforms)
+                .FirstOrDefaultAsync(g => g.Id == id);
+
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            // Récupérer la plateforme
+            var platform = await _context.Platform.FindAsync(platformId);
+            if (platform == null)
+            {
+                return NotFound();
+            }
+
+            // Initialiser la collection si nécessaire
+            game.Platforms ??= new List<Platform>();
+
+            // Éviter les doublons
+            if (!game.Platforms.Any(p => p.Id == platformId))
+            {
+                game.Platforms.Add(platform);
+                await _context.SaveChangesAsync();
+            }
+            ViewData["Platforms"] = new SelectList(_context.Platform.OrderBy(p => p.Name), "Id", "Name");
+            // Retour à la page de détails du jeu
+            return View("Edit", game);
+        }
+
+        // POST: Games/RemovePlatform
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemovePlatform(int gameId, int platformId)
+        {
+            // Charger le jeu avec ses plateformes actuelles
+            var game = await _context.Game
+                .Include(g => g.Platforms)
+                .FirstOrDefaultAsync(g => g.Id == gameId);
+
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            if (game.Platforms != null)
+            {
+                var toRemove = game.Platforms.FirstOrDefault(p => p.Id == platformId);
+                if (toRemove != null)
+                {
+                    game.Platforms.Remove(toRemove);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            // Retour à la page de détails du jeu
+            return View("Edit", game);
+        }
+
+        // GET: Games/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Game
+                .Include(g => g.Genre)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var game = await _context.Game.FindAsync(id);
+            if (game != null)
+            {
+                _context.Game.Remove(game);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GameExists(int id)
+        {
+            return _context.Game.Any(e => e.Id == id);
+        }
+
+        
+    }
+}
